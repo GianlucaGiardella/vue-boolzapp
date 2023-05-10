1d8ead95d81178c4786ec07e52f85781c2759265
GianluGiarde <gianlugiarde@gmail.com>
2023-05-10 15:13:43 +0200

    Change contacts names

--- js/vue-script.js
+++ js/vue-script.js
@@ -3,55 +3,55 @@ const app = Vue.createApp({
         return {
             profile: {
                 name: "Gianluca Giardella",
-                avatar: "./img/avatar_io.png"
+                avatar: "img/avatar_io.jpg"
             },
 
             contacts: [
                 {
-                    name: "Andrea",
-                    avatar: "./img/avatar_1.png",
+                    name: "Simone",
+                    avatar: "img/avatar_1.jpg",
                     visible: true,
                     messages: [],
                 },
                 {
-                    name: "Annalisa",
-                    avatar: "./img/avatar_2.png",
+                    name: "Erica",
+                    avatar: "img/avatar_2.jpg",
                     visible: true,
                     messages: [],
                 },
                 {
                     name: "Federico",
-                    avatar: "./img/avatar_3.png",
+                    avatar: "img/avatar_3.jpg",
                     visible: true,
                     messages: [],
                 },
                 {
                     name: "Marco",
-                    avatar: "./img/avatar_4.png",
+                    avatar: "img/avatar_4.jpg",
                     visible: true,
                     messages: [],
                 },
                 {
                     name: "Valerio",
-                    avatar: "./img/avatar_5.png",
+                    avatar: "img/avatar_5.jpg",
                     visible: true,
                     messages: [],
                 },
                 {
                     name: "Lara",
-                    avatar: "./img/avatar_6.png",
+                    avatar: "img/avatar_6.jpg",
                     visible: true,
                     messages: [],
                 },
                 {
                     name: "Lorenzo",
-                    avatar: "./img/avatar_7.png",
+                    avatar: "img/avatar_7.jpg",
                     visible: true,
                     messages: [],
                 },
                 {
                     name: "Giulio",
-                    avatar: "./img/avatar_8.png",
+                    avatar: "img/avatar_8.jpg",
                     visible: true,
                     messages: [],
                 },
