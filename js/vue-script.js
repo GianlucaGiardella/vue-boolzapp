6da4ee1ba4b11859db34848c8ca9683eded3f7a6
GianluGiarde <gianlugiarde@gmail.com>
2023-05-11 14:46:27 +0200

    Add last message time

--- js/vue-script.js
+++ js/vue-script.js
@@ -13,17 +13,17 @@ const app = Vue.createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2023 15:30:55',
                             text: 'Hai portato a spasso il cane?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2023 15:50:00',
                             text: 'Ricordati di stendere i panni',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 16:15:22',
+                            date: '10/01/2023 16:15:22',
                             text: 'Tutto fatto!',
                             status: 'received'
                         }
@@ -35,17 +35,17 @@ const app = Vue.createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '20/03/2020 16:30:00',
+                            date: '20/03/2023 16:30:00',
                             text: 'Ciao come stai?',
                             status: 'sent'
                         },
                         {
-                            date: '20/03/2020 16:30:55',
+                            date: '20/03/2023 16:30:55',
                             text: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received'
                         },
                         {
-                            date: '20/03/2020 16:35:00',
+                            date: '20/03/2023 16:35:00',
                             text: 'Mi piacerebbe ma devo andare a fare la spesa.',
                             status: 'sent'
                         }
@@ -57,17 +57,17 @@ const app = Vue.createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '28/03/2020 10:10:40',
+                            date: '28/03/2023 10:10:40',
                             text: 'La Marianna va in campagna',
                             status: 'received'
                         },
                         {
-                            date: '28/03/2020 10:20:10',
+                            date: '28/03/2023 10:20:10',
                             text: 'Sicuro di non aver sbagliato chat?',
                             status: 'sent'
                         },
                         {
-                            date: '28/03/2020 16:15:22',
+                            date: '28/03/2023 16:15:22',
                             text: 'Ah scusa!',
                             status: 'received'
                         }
@@ -79,12 +79,12 @@ const app = Vue.createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2023 15:30:55',
                             text: 'Lo sai che ha aperto una nuova pizzeria?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2023 15:50:00',
                             text: 'Si, ma preferirei andare al cinema',
                             status: 'received'
                         }
@@ -96,12 +96,12 @@ const app = Vue.createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2023 15:30:55',
                             text: 'Ricordati di chiamare la nonna',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2023 15:50:00',
                             text: 'Va bene, stasera la sento',
                             status: 'received'
                         }
@@ -113,17 +113,17 @@ const app = Vue.createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
-                            text: 'Ciao Claudia, hai novità?',
+                            date: '10/01/2023 15:30:55',
+                            text: 'Ciao Lara, hai novità?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2023 15:50:00',
                             text: 'Non ancora',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '10/01/2023 15:51:00',
                             text: 'Nessuna nuova, buona nuova',
                             status: 'sent'
                         }
@@ -135,12 +135,12 @@ const app = Vue.createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2023 15:30:55',
                             text: 'Fai gli auguri a Martina che è il suo compleanno!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2023 15:50:00',
                             text: 'Grazie per avermelo ricordato, le scrivo subito!',
                             status: 'received'
                         }
@@ -152,17 +152,17 @@ const app = Vue.createApp({
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2023 15:30:55',
                             text: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2023 15:50:00',
                             text: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '10/01/2023 15:51:00',
                             text: 'OK!!',
                             status: 'received'
                         }
@@ -176,6 +176,9 @@ const app = Vue.createApp({
     methods: {
         lastMessage(contact) {
             return contact.messages[contact.messages.length -1].text;
+        },
+        lastMessageTime(contact){
+            return contact.messages[contact.messages.length -1].date.slice(0, 10);
         }
     },
 });
